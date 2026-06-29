2e63926fc8192a6f4619abbc86ad5504e2d44c03
Moisés Alejandro Morán Huiza <00023426@uca.edu.sv>
2026-06-28 22:22:33 -0600

    M

---
+++ maincasino.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "fcasino.h"
+using namespace std;
+
+int opc;
+int main (){
+    mainmenu();
+    cin>>opc;
+    switch (opc){
+        case 1:
+
+        break;
+        case 2:
+
+        break;
+        case 3:
+
+        break;
+        case 4:
+
+        break;
+        default:
+
+        break;
+    }
+
+    return 0;
+}
