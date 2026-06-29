d4062279a239e354ba641095db5bfbf6e17d7a92
Moisés Alejandro Morán Huiza <00023426@uca.edu.sv>
2026-06-28 22:19:43 -0600

    F

---
+++ fcasino.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "fcasino.h"
+using namespace std;
+
+void mainmenu (){
+    cout<<"\n-------------------------------------------\n"<<"|           Bienvenido al UCAsino         |\n"<<"-------------------------------------------"
+    <<endl<<" ___________________________\n"<<"/ Pulsa 1 Para jugar Craps /\n"<<"---------------------------\n"
+    <<" ______________________________________________\n"<<"/ Pulsa 2 Para juagar a la maquinatragaperras /\n"<<"----------------------------------------------\n"
+    <<" _________________________________\n"<<"/ Pulsa 3 Para jugar a la ruleta /\n"<<"---------------------------------\n"
+    <<" _________________________________\n"<<"/ Pulsa 4 Para recargar creditos /\n"<<"---------------------------------\n" 
+    <<" _____________________________\n"<<"/ Pulsa 5 Cerrar el programa /\n"<<"-----------------------------\n"
+    << "Disclaimer: Si no quieres ganar dinero, por favor cierra este programa ¡YAAAAA!\n";  
+}
+void crapsmenu (){
+    cout<<"Bienvenido a Craps\n"<<"Empecemo\n"; 
+}
+
